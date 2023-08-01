91b115fd17f6788a16a1665cd716f67bebab3702
Green0wl <kolomietsivan88@gmail.com>
2023-08-01 12:08:40 +0200

    treesitter configured

---
+++ lua/configurations/plugins/nvim_treesitter_configs/nvim_treesitter_conf.lua
@@ -0,0 +1,83 @@
+local M = {}
+function M.config_func()
+  local configs = require("nvim-treesitter.configs")
+
+  configs.setup({
+    ensure_installed = require("configurations.plugins.nvim_treesitter_configs.ensure_installed_treesitter"),
+    auto_install = true,
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-space>',
+        node_incremental = '<c-space>',
+        scope_incremental = '<c-s>',
+        node_decremental = '<M-space>',
+      },
+    },
+    autopairs = {
+      enable = true,
+    },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = true,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      }
+    }
+  })
+
+  -- for nvim-treesitter folding
+  vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
+    group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+    callback = function()
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
+    end
+  })
+end
+
+return M
