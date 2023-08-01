d99b337775c6e5b2a995f60f4c20c65e2e9f8438
Green0wl <kolomietsivan88@gmail.com>
2023-08-01 18:12:18 +0200

    added hint/wrror/warring icons to nvim-tree plugin

--- lua/configurations/plugins/nvim_tree.lua
+++ lua/configurations/plugins/nvim_tree.lua
@@ -9,19 +9,58 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-	  require("nvim-tree").setup {
-		  on_attach = require("configurations.plugins.nvim_tree.nvim_tree_mappings").on_attach,
-		  sort_by = "case_sensitive",
-		  view = {
-			  width = 30,
-		  },
-		  renderer = {
-			  group_empty = true,
-		  },
-		  filters = {
-			  dotfiles = true,
-		  },
-
-	  }
+    require("nvim-tree").setup {
+      on_attach = require("configurations.plugins.nvim_tree.nvim_tree_mappings").on_attach,
+      sort_by = "case_sensitive",
+      view = {
+        width = 35,
+      },
+      renderer = {
+        group_empty = true,
+        root_folder_modifier = ":t",
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+      filters = {
+        dotfiles = true,
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+    }
   end,
 }
