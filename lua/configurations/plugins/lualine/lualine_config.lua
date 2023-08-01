02dbf12a8224d866a9d8aefcade7140044e64979
Green0wl <kolomietsivan88@gmail.com>
2023-08-01 13:05:20 +0200

    lualine configured

---
+++ lua/configurations/plugins/lualine/lualine_config.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+local time_table = {
+  'datetime',
+  style = '%H:%M'
+}
+
+M.custom_config = {
+  options = {
+    icons_enabled = true,
+    theme = 'tokyonight',
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { "mode", time_table }
+  }
+}
+
+return M
