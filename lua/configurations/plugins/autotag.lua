5ff10bf391f0947e3951d964be31f75ffdf67cb5
Green0wl <kolomietsivan88@gmail.com>
2023-08-21 14:42:53 +0200

    lsp_lines plugin added and configured.

--- lua/configurations/plugins/autotag.lua
+++ lua/configurations/plugins/autotag.lua
@@ -7,10 +7,6 @@ return {
       vim.lsp.diagnostic.on_publish_diagnostics,
       {
         underline = true,
-        virtual_text = {
-          spacing = 5,
-          severity_limit = 'Warning',
-        },
         update_in_insert = true,
       }
     )
