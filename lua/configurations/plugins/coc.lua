b4352a81df3006d7fc4b9d628ef9eecb9778adc2
Green0wl <kolomietsivan88@gmail.com>
2023-08-23 19:36:37 +0200

    pyright configured (pure).

--- lua/configurations/plugins/coc.lua
+++ lua/configurations/plugins/coc.lua
@@ -3,7 +3,7 @@ return {
   branch = 'release',
   init = function ()
     vim.g.coc_start_at_startup = 1
-    vim.g.coc_config_home = 'lua/configurations/plugins/coc/'
+    vim.g.coc_config_home = '$HOME/.config/nvim/lua/configurations/plugins/coc/'
     vim.g.coc_global_extensions = require("configurations.plugins.coc.coc_ensure_installed")
   end,
   config = function()
