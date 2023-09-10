a91c33adc301c2d1bd49918f2da24034cf492496
Green0wl <kolomietsivan88@gmail.com>
2023-09-10 13:09:10 +0200

    Configuring `cmake-language-server` (autoinstall it).

--- lua/configurations/lsp/settings/cmake.lua
+++ lua/configurations/lsp/settings/cmake.lua
@@ -1,1 +1,19 @@
+local function async_system(command)
+  local job_id = vim.fn.jobstart(command, {
+    on_stdout = function(_, data, _)
+      vim.schedule(function()
+        -- Проверка, что data является строкой Lua
+        if type(data) == "string" then
+          vim.api.nvim_out_write(data)
+        else
+          -- Преобразование data в строку Lua
+          vim.api.nvim_out_write(tostring(data))
+        end
+      end)
+    end
+  })
+  vim.fn.chansend(job_id, "")
+end
+
+async_system("pip install cmake-language-server")
 return {}
