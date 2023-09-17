2a4aff1a488e9d0073fc16849f2b3973abaffe1e
Green0wl <kolomietsivan88@gmail.com>
2023-09-17 17:41:08 +0200

    fix(plugins): Fix `resession.nvim` loading and saving autocmd

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -226,18 +226,25 @@ function M.resession_keymap_setup(resession)
   vim.keymap.set('n', '<leader>sl', resession.load)
   vim.keymap.set('n', '<leader>sd', resession.delete)
 
+  local function get_git_root()
+    local dot_git_path = vim.fn.finddir(".git", ".;")
+    return vim.fn.fnamemodify(dot_git_path, ":h")
+  end
+
   vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
       -- Only load the session if nvim was started with no args
       if vim.fn.argc(-1) == 0 then
+        local resessionLoadPath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
         -- Save these to a different directory, so our manual sessions don't get polluted
-        resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+        resession.load(resessionLoadPath, { dir = "dirsession", silence_errors = true })
       end
     end,
   })
   vim.api.nvim_create_autocmd("VimLeavePre", {
     callback = function()
-      resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
+      local resessionSavePath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
+      resession.save(resessionSavePath, { dir = "dirsession", notify = false })
     end,
   })
 end
