3ac02b435e2c417be1450951cdd9b0b11d0eaced
Green0wl <kolomietsivan88@gmail.com>
2023-09-19 15:25:49 +0200

    fix(notify): Ban broken `notify.nvim` messages

--- lua/configurations/plugins/nvim_notify.lua
+++ lua/configurations/plugins/nvim_notify.lua
@@ -2,6 +2,16 @@ return {
   "rcarriga/nvim-notify",
   lazy = false,
   config = function()
-    vim.notify = require("notify")
+    local banned_messages = { "No information available" }
+
+    -- A workaround from https://github.com/neovim/nvim-lspconfig/issues/1931
+    vim.notify = function(msg, ...)
+      for _, banned in ipairs(banned_messages) do
+        if msg == banned then
+          return
+        end
+      end
+      return require("notify")(msg, ...)
+    end
   end,
 }
