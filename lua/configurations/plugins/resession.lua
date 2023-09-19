db8b8e37e9ebe3ad991c31b003270a0b1868de71
Green0wl <kolomietsivan88@gmail.com>
2023-09-19 14:16:21 +0200

    perf(resession): Move autocmd to `resession.lua` file

--- lua/configurations/plugins/resession.lua
+++ lua/configurations/plugins/resession.lua
@@ -12,5 +12,27 @@ return {
     })
 
     require("core.mapping").resession_keymap_setup(resession)
+
+    local function get_git_root()
+      local dot_git_path = vim.fn.finddir(".git", ".;")
+      return vim.fn.fnamemodify(dot_git_path, ":h")
+    end
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        -- Only load the session if nvim was started with no args
+        if vim.fn.argc(-1) == 0 then
+          local resessionLoadPath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
+          -- Save these to a different directory, so our manual sessions don't get polluted
+          resession.load(resessionLoadPath, { dir = "dirsession", silence_errors = true })
+        end
+      end,
+    })
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      callback = function()
+        local resessionSavePath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
+        resession.save(resessionSavePath, { dir = "dirsession", notify = false })
+      end,
+    })
   end
 }
--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -225,28 +225,6 @@ function M.resession_keymap_setup(resession)
   vim.keymap.set('n', '<leader>ss', resession.save)
   vim.keymap.set('n', '<leader>sl', resession.load)
   vim.keymap.set('n', '<leader>sd', resession.delete)
-
-  local function get_git_root()
-    local dot_git_path = vim.fn.finddir(".git", ".;")
-    return vim.fn.fnamemodify(dot_git_path, ":h")
-  end
-
-  vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-      -- Only load the session if nvim was started with no args
-      if vim.fn.argc(-1) == 0 then
-        local resessionLoadPath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
-        -- Save these to a different directory, so our manual sessions don't get polluted
-        resession.load(resessionLoadPath, { dir = "dirsession", silence_errors = true })
-      end
-    end,
-  })
-  vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function()
-      local resessionSavePath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
-      resession.save(resessionSavePath, { dir = "dirsession", notify = false })
-    end,
-  })
 end
 
 return M
