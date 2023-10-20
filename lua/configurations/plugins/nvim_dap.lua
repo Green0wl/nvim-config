6c4cabcc990956ab9c2a3e9a1e31fd3bc75a50df
Green0wl <kolomietsivan88@gmail.com>
2023-10-20 19:08:35 +0200

    feat(nvim-dap): Add `config` function

--- lua/configurations/plugins/nvim_dap.lua
+++ lua/configurations/plugins/nvim_dap.lua
@@ -1,4 +1,11 @@
 return {
   "mfussenegger/nvim-dap",
   lazy = true,
+  config = function()
+    local dap = require('dap')
+    dap.defaults.fallback.external_terminal = {
+      command = '/usr/bin/kitty',
+      args = { '-e' },
+    }
+  end
 }
