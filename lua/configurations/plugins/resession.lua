0970e625271cc3290bbb8a280fcbb60ab174a097
Green0wl <kolomietsivan88@gmail.com>
2024-05-24 15:39:03 +0200

    fix(resession): fix loading error during drastic changes in project structure when past folders no longer exist in the same repository

--- lua/configurations/plugins/resession.lua
+++ lua/configurations/plugins/resession.lua
@@ -18,22 +18,27 @@ return {
       return vim.fn.fnamemodify(dot_git_path, ":h")
     end
 
+    local function saveSession()
+      local resessionSavePath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
+      resession.save(resessionSavePath, { dir = "dirsession", notify = false })
+    end
+
     vim.api.nvim_create_autocmd("VimEnter", {
       callback = function()
         -- Only load the session if nvim was started with no args
         if vim.fn.argc(-1) == 0 then
           local resessionLoadPath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
           -- Save these to a different directory, so our manual sessions don't get polluted
-          resession.load(resessionLoadPath, { dir = "dirsession", silence_errors = true })
+          local success = pcall(function()
+            resession.load(resessionLoadPath, { dir = "dirsession", silence_errors = true })
+          end)
+          if not success then saveSession() end
         end
       end,
     })
     vim.api.nvim_create_autocmd("VimLeavePre", {
       callback = function()
-        if vim.fn.argc(-1) == 0 then
-          local resessionSavePath = (get_git_root() == ".") and vim.fn.getcwd() or get_git_root()
-          resession.save(resessionSavePath, { dir = "dirsession", notify = false })
-        end
+        if vim.fn.argc(-1) == 0 then saveSession() end
       end,
     })
   end
